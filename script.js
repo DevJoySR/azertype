99b53d1c2556e4d7faaa69d0f6e7c5f7d7a3a90b
DevJoySR <joysr@proton.me>
2025-10-17 08:35:16 +0200

    feat: index.html/script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+
+let motTapeOk = true
+if (motTapeOk)
+{
+    console.log("Bravo, vous avez correctement tapé le mot");
+} else{
+    console.log("Echec, le mot n'est pas correct")
+}
