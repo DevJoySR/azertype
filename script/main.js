6c0e4c4333df33488956bc506dad8fe98d6aca7c
DevJoySR <joysr@proton.me>
2026-03-06 08:34:55 +0100

    update js

--- script/main.js
+++ script/main.js
@@ -1,6 +1,11 @@
-// lancerJeu()
-let inputEcriture = document.getElementById("inputEcriture")
-let btnValiderMot = document.getElementById("btnValiderMot")
-console.log(inputEcriture, btnValiderMot)
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
 
-let scoreAfficher = document.querySelector()
+// Ici, j'ai mis la fonction lancerJeu() en commentaire pour ne pas être dérangé par l'affichage des popups. 
+// lancerJeu()
+
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
