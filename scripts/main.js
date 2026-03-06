5c2d79f1c3e779c1dc3b136f014aa2c5859a047c
DevJoySR <joysr@proton.me>
2026-03-06 08:47:32 +0100

    fix

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+/*********************************************************************************
+ * 
+ * Point d'entrée, c'est lui qui intialise le jeu et lance la boucle de jeu. 
+ * 
+ *********************************************************************************/
+
+// Ici, j'ai mis la fonction lancerJeu() en commentaire pour ne pas être dérangé par l'affichage des popups. 
+// lancerJeu()
+
+let inputEcriture = document.getElementById("inputEcriture")
+console.log(inputEcriture)
