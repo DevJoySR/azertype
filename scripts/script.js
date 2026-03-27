a9b0711f09d2bca6391224d0d7d5c6f3110fd58c
DevJoySR <joysr@proton.me>
2026-03-27 14:01:33 +0100

    update form

--- scripts/script.js
+++ scripts/script.js
@@ -13,7 +13,7 @@ function afficherResultat(score, nbMotsProposes) {
     // Récupération de la zone dans laquelle on va écrire le score
     let spanScore = document.querySelector(".zoneScore span")
     // Ecriture du texte
-    let affichageScore = `${score} / ${nbMotsProposes}` 
+    let affichageScore = `${score} / ${nbMotsProposes}`
     // On place le texte à l'intérieur du span. 
     spanScore.innerText = affichageScore
 }
@@ -38,6 +38,36 @@ function afficherEmail(nom, email, score) {
     let mailto = `mailto:${email}?subject=Partage du score Azertype&body=Salut, je suis ${nom} et je viens de réaliser le score ${score} sur le site d'Azertype !`
     location.href = mailto
 }
+
+function verifierMail(balise) {
+    let emailRegExp = new RegPex("[a-z._-]+@[a-z._-]+\.+[a-z._-]+")
+    if (emailRegExp.test(balise.value)) {
+        console.log("ok");
+        balise.classList.remove("error")
+    } else {
+        balise.classList.add("error")
+        console.log("ko")
+    }
+}
+
+function validerNom(nom) {
+    if (nom.length >= 2) {
+        return true
+    } else {
+        return false
+    }
+}
+
+function validerMail(email) {
+    let emailRegExp = new RegPex("[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}")
+    if (emailRegExp.test(email)) {
+        return true
+    }
+    return false
+}
+
+
+
 
 /**
  * Cette fonction lance le jeu. 
@@ -87,6 +117,21 @@ function lancerJeu() {
             afficherProposition(listeProposition[i])
         })
     }
+    let form = document.querySelector("form")
 
+    form.addEventListener("submit", (event) => {
+        event.preventDefault()
+        let baliseNom = document.getElementById("nom")
+        let nom = baliseNom.value
+        let baliseEmail = document.getElementById("email")
+        let email = baliseEmail.value
+
+        if (validerNom(nom) && validerMail(email)) {
+            let scoreMail = '${score} / ${i}'
+            afficherEmail(nom, email, scoreMail)
+        } else {
+            console.log("Erreur")
+        }        
+    })
     afficherResultat(score, i)
 }
