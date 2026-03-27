656447bbd8e878e9692e2ecb91e836d242d10c15
DevJoySR <joysr@proton.me>
2026-03-27 10:38:36 +0100

    update

--- scripts/script.js
+++ scripts/script.js
@@ -18,42 +18,9 @@ function afficherResultat(score, nbMotsProposes) {
     spanScore.innerText = affichageScore
 }
 
-/**
- * Cette fonction demande à l'utilisateur de choisir entre "mots" et "phrases" et retourne le choix de l'utilisateur
- * @return {string} : le choix de l'utilisateur, ce choix est nécessairement "mots" ou "phrases
- */
-// function choisirPhrasesOuMots() {
-//     // Tant que l'utilisateur n'a pas saisi "mots" ou "phrases", on lui redemande de saisir un choix
-//     let choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-//     while (choix !== "mots" && choix !== "phrases") {
-//         choix = prompt("Avec quelle liste désirez-vous jouer : 'mots' ou 'phrases' ?")
-//     }
-//     return choix
-// }
-
-/**
- * Cette fonction lance la boucle de jeu, c'est à dire qu'elle demande à l'utilisateur de saisir tous les mots
- * contenus dans le tableau listePropositions. A chaque mot saisi, on incrémente le score de l'utilisateur
- * 
- * @param {array[string]} listePropositions 
- * @return {number} : le score de l'utilisateur
- */
-// function lancerBoucleDeJeu(listePropositions) {
-//     let score = 0
-//     for (let i = 0; i < listePropositions.length; i++) {
-//         // On demande à l'utilisateur de saisir le mot correspondant à l'indice i
-//         let motUtilisateur = prompt("Entrez le mot : " + listePropositions[i])
-//         if (motUtilisateur === listePropositions[i]) {
-//             // Si le mot saisi par l'utilisateur est correct, on incrémente le score
-//             score++
-//         }
-//     }
-//     return score
-// }
-
-function afficherProposition(motAAfficher) {
+function afficherProposition(proposition) {
     let zoneProposition = document.querySelector(".zoneProposition")
-    zoneProposition.innerText = motAAfficher
+    zoneProposition.innerText = proposition
 }
 
 /**
@@ -65,26 +32,24 @@ function lancerJeu() {
     let score = 0
     let i = 0
 
-    let btnValiderMot = document.getElementById("btnValiderMot");
-    let inputEcriture = document.getElementById("inputEcriture");
-    afficherProposition(listeMots[i]    )
-
+    let btnValiderMot = document.getElementById("btnValiderMot")
+    let inputEcriture = document.getElementById("inputEcriture")
+    afficherProposition(listeMots[i])
     btnValiderMot.addEventListener("click", () => {
-        console.log(i, inputEcriture.value)
+        console.log(inputEcriture.value)
         if (inputEcriture.value === listeMots[i]) {
             score++
         }
-        console.log(listeMots[i])
         i++
         afficherResultat(score, i)
-        inputEcriture.value = "";
-
+        inputEcriture.value = ''
         if (listeMots[i] === undefined) {
             afficherProposition("Le jeu est fini")
-            btnValiderMot.disabled = true;
+            btnValiderMot.disabled = true
         } else {
             afficherProposition(listeMots[i])
         }
+        
     })
 
     afficherResultat(score, i)
